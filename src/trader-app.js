195a2b5e90c08338b2b7a186b58d97020de7ac5c
Veera <veera.k.sundar@gmail.com>
2017-06-04 10:17:38 -0700

    Inital setup.

---
+++ src/trader-app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class TraderApp extends Component {
+
+    render() {
+        return (
+            <div classname="app">
+                Trader App
+            </div>
+        )
+    }
+}
+
+export default TraderApp;
