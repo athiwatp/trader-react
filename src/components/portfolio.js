75c704559d5b51e07b93288158f1e3871dac89e8
Veera <veera.k.sundar@gmail.com>
2017-06-12 14:11:09 -0700

    UI Changes.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,34 @@
 import React, {Component} from "react";
 import Stock from "./stock";
+import Utils from '../utils';
+import classnames from 'classnames';
+import './portfolio.css';
 
 class Portfolio extends Component {
 
     render() {
+
+        let wealth = this.props.stocks.reduce((memo, s) => {
+            memo.invested += (s.qty * s.price);
+            memo.current += (s.qty * s.currentPrice);
+            return memo;
+        }, {invested: 0, current: 0});
+
+        let wealthChange = wealth.current - wealth.invested;
+
+
+        let wealthChangeClassNames = classnames({
+            'wealth-change': true,
+            'volatile-value': true,
+            up: wealthChange > 0,
+            down: wealthChange < 0
+        });
+
         return (
             <div className="portfolio">
-                <div className="summary">
-
+                <div className="wealth">
+                    <em className="current">{Utils.currency(wealth.current)}</em>
+                    <em className={wealthChangeClassNames}> {wealthChange.toLocaleString()}</em>
                 </div>
                 {
                     this.props.stocks.map((stock) => <Stock key={stock.id} stock={stock} {...this.props} />)
